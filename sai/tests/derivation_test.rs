7d418738de5e9e50d26f385b5115d6ab1eda8f81
Edyta Pawlak <edyta.pawlakk@gmail.com>
2023-03-24 12:37:14 +0100

    Add tests folder

---
+++ sai/tests/derivation_test.rs
@@ -0,0 +1,18 @@
+use cesrox::primitives::codes::self_addressing::SelfAddressing;
+use sai::derivation::SelfAddressingCode;
+
+
+#[test]
+fn test_derive() {
+    let data = "hello there";
+    let code: SelfAddressingCode = SelfAddressing::Blake3_256.into();
+    let sai = code.derive(data.as_bytes());
+
+    assert_eq!(
+        format!("{}", sai),
+        "ENmwqnqVxonf_bNZ0hMipOJJY25dxlC8eSY5BbyMCfLJ"
+    );
+    assert!(sai.verify_binding(data.as_bytes()));
+    assert!(!sai.verify_binding("wrong data".as_bytes()));
+}
+

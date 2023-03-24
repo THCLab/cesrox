7971f257b6456ebba281c9d60195b3eed7bbe0b8
Edyta Pawlak <edyta.pawlakk@gmail.com>
2023-03-24 12:31:53 +0100

    Add bin

---
+++ sai/README.md
@@ -0,0 +1,71 @@
+# Self-Addressing Identifier
+
+A Rust implementation of the [IETF Draft SAID specification](https://weboftrust.github.io/ietf-said/draft-ssmith-said.html).
+
+Self-Addressing Identifier (SAI) provides a compact text representation of digests of data.
+It supports multiple hash algorithms.
+
+Self Addressing Identifier is base64 encoded hash of data, prepended with
+one or two letter prefix, called derivation code. Derivation code determines
+which hash algorithm was used to derive the digest.
+
+| derivation code| digest type 		| code length 	| identifier length	|
+|---------------|-------------------|---------------|-------------------|
+| E				| Blake3-256 Digest | 1				| 44 				|
+| F 			| Blake2b-256 Digest| 1				| 44				|
+| G				| Blake2s-256 Digest| 1				| 44				|
+| H				| SHA3-256 Digest 	| 1				| 44				|
+| I				| SHA2-256 Digest	| 1				| 44				|
+| 0D			| Blake3-512 Digest | 2				| 88				|
+| 0E			| Blake2b-512 Digest| 2				| 88				|
+| 0F			| SHA3-512 Digest 	| 2				| 88				|
+| 0G			| SHA2-512 Digest	| 2				| 88				|
+
+## License
+
+EUPL 1.2 
+
+We have distilled the most crucial license specifics to make your adoption seamless: [see here for details](https://github.com/THCLab/licensing).
+
+## Usage
+
+To derive Self Addressing Identifier, `SelfAddressing` enum can be used. It
+determines hashing algorithm used for Identifier derivation.  Here are its
+possible values:
+
+```rust
+pub enum SelfAddressing {
+    Blake3_256,
+    Blake2B256(Vec<u8>),
+    Blake2S256(Vec<u8>),
+    SHA3_256,
+    SHA2_256,
+    Blake3_512,
+    SHA3_512,
+    Blake2B512,
+    SHA2_512,
+}
+```
+
+`verify_bindings` function checks if provided data matches digest encoded in Self Addressing Identifier.
+
+### Example
+```rust
+let data = "hello there";
+let sai = SelfAddressing::Blake3_256.derive(data.as_bytes());
+
+assert_eq!(format!("{}", sai), "ENmwqnqVxonf_bNZ0hMipOJJY25dxlC8eSY5BbyMCfLJ");
+assert!(sai.verify_binding(data.as_bytes()));
+assert!(!sai.verify_binding("wrong data".as_bytes()));
+```
+
+## wasm
+`bindings/sai_wasm` directory contains a project which allows to compile sai library to wasm.
+
+## Releasing new version
+[cargo-release](https://github.com/crate-ci/cargo-release) is required
+
+To release new version run `cargo release`
+
+Due to [release config](./release.toml) it will bump version, create new git tag
+and push it to remote.
---
+++ sai/src/bin/sai.rs
@@ -0,0 +1,99 @@
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+use cesrox::primitives::CesrPrimitive;
+use clap::{App, Arg};
+use sai::derivation::SelfAddressingCode;
+use std::{fs, str::FromStr};
+
+fn main() {
+    let matches = App::new("SAI")
+        .version(VERSION)
+        .subcommand(
+            App::new("gen")
+                .about("Generate Self-Addressing Identifier")
+                .arg(
+                    Arg::new("data")
+                        .short('d')
+                        .long("data")
+                        .takes_value(true)
+                        .required_unless_present("file")
+                        .help("Source data against which we would like to calculate digest"),
+                )
+                .arg(
+                    Arg::new("file")
+                        .short('f')
+                        .long("file")
+                        .required_unless_present("data")
+                        .takes_value(true)
+                        .help(
+                            "File from which we would like to read data against which we would like to calculate digest"),
+                )
+                .arg(
+                    Arg::new("type")
+                        .short('t')
+                        .long("type")
+                        .takes_value(true)
+                        .required(true)
+                        .help(
+                            "Derevation code for the digest, algorithm used for digest.
+Supported codes:
+   E - Blake3_256
+   F - Blake2B256
+   G - Blake2S256
+   H - SHA3_256
+   I - SHA2_256
+   0D - Blake3_512
+   0E - SHA3_512
+   0F - Blake2B512
+   0G - SHA2_512",
+                        ),
+                ),
+        )
+        .subcommand(
+            App::new("verify")
+                .about("Verify SAI with provided data")
+                .arg(
+                    Arg::new("sai")
+                        .short('s')
+                        .long("sai")
+                        .takes_value(true)
+                        .required(true)
+                        .help("Digest against which we would like to verify the content"),
+                )
+                .arg(
+                    Arg::new("data")
+                        .short('d')
+                        .long("data")
+                        .takes_value(true)
+                        .required(true)
+                        .help("Source data against which we would like to verify given digest"),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("gen") {
+        let mut data = Vec::new();
+
+        if matches.contains_id("data") {
+            data.extend_from_slice(matches.value_of("data").unwrap().as_bytes());
+        }
+
+        if matches.contains_id("file") {
+            let file_path = matches.value_of("file").unwrap();
+            let file_data =
+                fs::read_to_string(file_path).expect("Something went wrong reading the file");
+            data.extend_from_slice(file_data.as_bytes())
+        }
+        let prefix_code = matches.value_of("type").unwrap();
+        let sai = SelfAddressingCode::from_str(prefix_code).unwrap();
+        let calculated_sai = sai.derive(&data).to_str();
+        println!("{}", calculated_sai);
+    }
+    if let Some(matches) = matches.subcommand_matches("verify") {
+        let data = matches.value_of("data").unwrap().as_bytes();
+        let sai = matches.value_of("sai").unwrap();
+        let prefix = SelfAddressingCode::from_str(sai).unwrap();
+        let calculated_sai = prefix.derive(data).to_str();
+        println!("{:?}", calculated_sai.eq(sai));
+    }
+}

8c1aff9f225b58e7f3310b51599520836dea5b1e
Edyta Pawlak <edyta.pawlakk@gmail.com>
2025-06-18 18:37:29 +0200

    refactor: move errors to separate module

--- cesr/src/error.rs
+++ cesr/src/error.rs
@@ -1,6 +1,10 @@
+use std::sync::mpsc::SendError;
+
 use base64::DecodeError;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+
+use crate::value::Value;
 
 #[derive(Error, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Error {
@@ -26,5 +30,36 @@ pub enum Error {
 impl From<base64::DecodeError> for Error {
     fn from(_: DecodeError) -> Self {
         Error::Base64DecodingError
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum CESRError {
+    #[error("Can't parse stream: {0}")]
+    ParsingError(ParsingError),
+
+    #[error(transparent)]
+    SendingError(#[from] SendError<Value>),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ParsingError {
+    #[error("Incomplete stream: {0}")]
+    IncompleteStream(String),
+    #[error("Error while parsing: {0}")]
+    Error(String),
+    #[error("Parsing failure: {0}")]
+    Failure(String),
+}
+
+impl From<nom::Err<nom::error::Error<&str>>> for ParsingError {
+    fn from(err: nom::Err<nom::error::Error<&str>>) -> Self {
+        match err {
+            nom::Err::Incomplete(_) => {
+                ParsingError::IncompleteStream("Stream is incomplete".to_string())
+            }
+            nom::Err::Error(e) => ParsingError::Error(e.to_string()),
+            nom::Err::Failure(e) => ParsingError::Failure(e.to_string()),
+        }
     }
 }
--- cesr/src/lib.rs
+++ cesr/src/lib.rs
@@ -3,9 +3,10 @@ pub mod error;
 pub mod group;
 pub mod payload;
 pub mod primitives;
-use std::sync::mpsc::SendError;
 use std::sync::mpsc::Sender;
 
+use crate::error::CESRError;
+use crate::error::ParsingError;
 use crate::value::parse_value;
 use crate::value::Value;
 
@@ -14,37 +15,6 @@ pub mod cesr_proof;
 pub mod conversion;
 pub mod universal_codes;
 pub mod value;
-
-#[derive(thiserror::Error, Debug)]
-pub enum CESRError {
-    #[error("Can't parse stream: {0}")]
-    ParsingError(ParsingError),
-
-    #[error(transparent)]
-    SendingError(#[from] SendError<Value>),
-}
-
-#[derive(Debug, thiserror::Error)]
-pub enum ParsingError {
-    #[error("Incomplete stream: {0}")]
-    IncompleteStream(String),
-    #[error("Error while parsing: {0}")]
-    Error(String),
-    #[error("Parsing failure: {0}")]
-    Failure(String),
-}
-
-impl From<nom::Err<nom::error::Error<&str>>> for ParsingError {
-    fn from(err: nom::Err<nom::error::Error<&str>>) -> Self {
-        match err {
-            nom::Err::Incomplete(_) => {
-                ParsingError::IncompleteStream("Stream is incomplete".to_string())
-            }
-            nom::Err::Error(e) => ParsingError::Error(e.to_string()),
-            nom::Err::Failure(e) => ParsingError::Failure(e.to_string()),
-        }
-    }
-}
 
 pub fn parse_one(stream: &str) -> Result<(&str, Value), ParsingError> {
     Ok(parse_value(stream)?)
